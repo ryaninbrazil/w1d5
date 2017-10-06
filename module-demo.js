7c4c7600f799ffb5e66d1b25a5fed1bc8e064b30
Ryan <ryaninbrazil@gmail.com>
2017-10-06 20:32:03 +0000

    packages calling exercise

---
+++ module-demo.js
@@ -0,0 +1,26 @@
+var romNumbers = {
+  one: 'i',
+  two: 'ii',
+  three: 'iii',
+  four: 'iv',
+  five: function () {
+    
+    return 'v';
+
+  },
+  six: vi
+};
+
+function vi() {
+  console.log("vi");
+}
+
+
+// romNumbers.five();
+// romNumbers.six();
+
+
+
+module.exports = {
+  romNumbers: romNumbers
+};
---
+++ module-demo2.js
@@ -0,0 +1,10 @@
+
+
+var numerial = require('./module-demo.js');
+
+
+
+
+
+console.log("This is the roman numerial for five: ", numerial.romNumbers.three);
+
